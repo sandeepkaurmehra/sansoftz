dc17484ee54850618e2a4136f6c991c732e5500d
Sandeep Kaur <sandeep@hansoftz.com>
2016-06-02 12:26:35 +0530

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/sansoftz/admin/');
+define('HTTP_CATALOG', 'http://localhost/sansoftz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/sansoftz/admin/');
+define('HTTPS_CATALOG', 'http://localhost/sansoftz/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/sansoftz/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/sansoftz/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/sansoftz/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/sansoftz/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/sansoftz/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/sansoftz/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/sansoftz/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/sansoftz/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/sansoftz/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/sansoftz/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/sansoftz/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/sansoftz/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sansoftz');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/sansoftz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/sansoftz/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/sansoftz/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/sansoftz/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/sansoftz/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/sansoftz/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/sansoftz/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/sansoftz/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/sansoftz/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/sansoftz/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/sansoftz/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/sansoftz/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/sansoftz/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sansoftz');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
